61122e3cf57ba4608a82f78a06041731260f6a31
Jeff Innes <jeffdango@gmail.com>
2019-01-15 16:29:01 -0700

    add original dog counter scropt for reference

---
+++ PSdogcounter.ps1
@@ -0,0 +1,16 @@
+﻿[int]$currentdogs = (Read-Host -Prompt "How many dogs do you have?")
+[int]$additionaldogs = Read-Host -Prompt "How many more dogs do you want?"
+[int]$totaldogs = $currentdogs + $additionaldogs
+
+Write-Host "Awesome!"
+
+    if ($currentdogs -eq 1){
+        Write-Host "You currently have $currentdogs dog.  If you got $additionaldogs more you would have $totaldogs dogs!"
+        Write-Host "I hope someday you have $totaldogs dogs!"
+        }
+
+    else {
+        Write-Host "You currently have $currentdogs dogs.  If you got $additionaldogs more you would have $totaldogs dogs!"
+        Write-Host "I hope someday you have $totaldogs dogs!"
+        }
+Read-Host "I hope you enjoyed this!  Press Enter to exit!" | Out-Null
